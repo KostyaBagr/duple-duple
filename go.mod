088504f81be798a4ba6b30f848af6e546d2bb4fa
root <root@DESKTOP-7JJIHQR>
2026-02-02 17:20:41 +0500

    added: interface for dbms backup

--- go.mod
+++ go.mod
@@ -42,6 +42,7 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.19.17 // indirect
 	github.com/aws/smithy-go v1.24.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/lib/pq v1.11.1
 	github.com/spf13/cobra v1.10.2 // indirect
 	github.com/spf13/pflag v1.0.10 // indirect
 )
--- internal/backup/common.go
+++ internal/backup/common.go
@@ -1,11 +1,13 @@
 package backup
 
 import (
+	"database/sql"
 	"errors"
 	"fmt"
 
 	cfg "github.com/KostyaBagr/duple-duple/internal/config"
 	"github.com/KostyaBagr/duple-duple/internal/utils"
+	_ "github.com/lib/pq"
 )
 
 // Generates full path. It local type of storage was selected all dump files will be uploaded
@@ -27,25 +29,75 @@ func dumpFullPath(fileName string) (path string) {
 	return cfg.TmpBackupPath + fileName
 }
 
+type DumpDBMS interface {
+	dump() (*DumpFileStats, string, error)
+	ping() error
+}
+
+type dumpPostgres struct {
+}
+
+// Create a postgres dump methdo
+func (p dumpPostgres) dump() (*DumpFileStats, string, error) {
+	stat, path, err := PostgresDump(
+		cfg.AppConfig.DBMS.Postgres.Host,
+		cfg.AppConfig.DBMS.Postgres.User,
+		cfg.AppConfig.DBMS.Postgres.Password,
+		cfg.AppConfig.DBMS.Postgres.DB,
+		cfg.AppConfig.DBMS.Postgres.Port,
+	)
+	if err != nil {
+		fmt.Printf("Error during postgres dump: %s", err)
+		return stat, "", err
+	}
+	return stat, path, nil
+}
+
+// Ping method
+func (p dumpPostgres) ping() error {
+	connStr := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s sslmode=disable",
+		cfg.AppConfig.DBMS.Postgres.Host,
+		cfg.AppConfig.DBMS.Postgres.Port,
+		cfg.AppConfig.DBMS.Postgres.User,
+		cfg.AppConfig.DBMS.Postgres.Password,
+	)
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		fmt.Printf("Failed to open database connection: %v", err)
+		return err
+	}
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		fmt.Printf("Database is not ready: %v", err)
+		return err
+	}
+
+	fmt.Println("Database is ready!")
+	return nil
+}
+
+var dbmsStructMapper = map[string]DumpDBMS{
+	cfg.Postgres.String(): dumpPostgres{},
+}
+
 // This is a discpatcher for dumping.
 // Based on dbms var it runs a piplelin
 // Returns DumpFileStats (such as time complexity,
 // file size and so on),
 // path to file (for storagies) and error (optional)
 func DumpDispatcher(dbms string) (*DumpFileStats, string, error) {
-
-	if dbms == cfg.Postgres.String() {
-		stat, path, err := PostgresDump(
-			cfg.AppConfig.DBMS.Postgres.Host,
-			cfg.AppConfig.DBMS.Postgres.User,
-			cfg.AppConfig.DBMS.Postgres.Password,
-			cfg.AppConfig.DBMS.Postgres.DB,
-			cfg.AppConfig.DBMS.Postgres.Port,
-		)
-		if err != nil {
-			return stat, "", errors.New("Unable to create postgres dump")
-		}
-		return stat, path, nil
+	dbmsDumpProcessor, ok := dbmsStructMapper[dbms]
+	if !ok {
+		fmt.Printf("Invalid DBMS type %s", dbms)
+		return &DumpFileStats{}, "", errors.New("Undefined dbms")
 	}
-	return &DumpFileStats{}, "", errors.New("Undefined dbms")
+	err := dbmsDumpProcessor.ping()
+	if err != nil {
+		return &DumpFileStats{}, "", err
+	}
+	return dbmsDumpProcessor.dump()
+
 }
